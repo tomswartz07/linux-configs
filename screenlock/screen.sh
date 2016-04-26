d8cc98dab32db2194b4508fb746ed07569a4d0fc
Tom Swartz <tom.swartz07@gmail.com>
2016-04-26 09:04:50 -0400

    Add trap function to screen.sh
    Add a trap exit function to prevent multiple instances of i3lock from
    spawning.
    
    Previously, i3lock would continue running in the background and chew up
    resources after several days of locking/unlocking where multiple
    instances were created.

--- screenlock/screen.sh
+++ screenlock/screen.sh
@@ -3,4 +3,9 @@ scrot /tmp/screen.png
 convert /tmp/screen.png -scale 10% -scale 1000% /tmp/screen.png
 [[ -f $1 ]] && convert /tmp/screen.png $1 -geometry +350+512 -composite -matte /tmp/screen.png
 i3lock -u -i /tmp/screen.png
-rm /tmp/screen.png
+
+function finish {
+        kill i3lock
+        rm /tmp/screen.png
+}
+trap finish EXIT
