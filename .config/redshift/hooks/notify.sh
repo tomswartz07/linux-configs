5893bf3bc239bca294b8894d76b447f0c7cffbc2
Tom Swartz <tom.swartz07@gmail.com>
2019-11-20 09:50:52 -0500

    Add day/night icons to notifications

--- .config/redshift/hooks/notify.sh
+++ .config/redshift/hooks/notify.sh
@@ -1,5 +1,18 @@
 #!/bin/sh
-case $1 in
-        period-changed)
-                exec notify-send "Redshift" "Period changed to $3"
-esac
+
+# Set up custom icons
+icon_night="weather-clear-night-symbolic"
+icon_day="weather-clear-symbolic"
+if [ "$1" = period-changed ]; then
+        case $3 in
+                night)
+                        exec notify-send "Redshift" "Period changed to $3" --icon="${icon_night}"
+                        ;;
+                transition)
+                        exec notify-send "Redshift" "Period changed to $3"
+                        ;;
+                daytime)
+                        exec notify-send "Redshift" "Period changed to $3" --icon="${icon_day}"
+                        ;;
+        esac
+fi
