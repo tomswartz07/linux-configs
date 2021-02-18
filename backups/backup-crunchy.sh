d448a5e3e9974be1eac994448b2ad12a98803dcf
Tom Swartz <tom.swartz07@gmail.com>
2021-02-17 21:55:05 -0500

    Fixup backup args

--- backups/backup-crunchy.sh
+++ backups/backup-crunchy.sh
@@ -4,7 +4,7 @@
 
 RSYNC_BIN=/usr/bin/rsync
 BACKUP_DIR=/mnt/Storage/Backup/crunchybackup
-RSYNC_OPTIONS="-l -q -a --recursive --delete --times --perms --owner --group --update --checksum --links --compress --protect-args"
+RSYNC_OPTIONS="-q -a --recursive --delete --times --perms --owner --group --update --checksum --links --compress --protect-args"
 
 if [ -d ${BACKUP_DIR} ]; then
         ${RSYNC_BIN} ${RSYNC_OPTIONS} /mnt/Storage/Backup/crunchy/ ${BACKUP_DIR}
