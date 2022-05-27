2641b8d42ad4d6347cec7a8a9655311477b121b0
Tom Swartz <tom.swartz07@gmail.com>
2022-05-27 10:40:11 -0400

    Remove more yay dev packages

--- backups/clean-yay-cache.sh
+++ backups/clean-yay-cache.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cachedirs="$(find ~/.cache/yay -maxdepth 1 -type d | awk '{ print "-c " $1 }' | tail -n +2)"
 echo "==> clean AUR package cache"
-/usr/bin/paccache -rvk3 $cachedirs
+/usr/bin/paccache -rvk2 $cachedirs
